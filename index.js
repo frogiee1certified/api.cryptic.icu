22877076838083ff95492e1d353bf10e162cfcac
frogiee1certified <122134800+frogiee1certified@users.noreply.github.com>
2024-09-16 20:43:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
-        res.status(200).json({ endpoints: 'api.cryptic.icu/dns/{hostname}, api.cryptic.icu/domain/ssl/{hostname},  api.cryptic.icu/webshot/{url)'});
+        res.status(200).json({ endpoints: 'api.cryptic.icu/dns/example.com, api.cryptic.icu/domain/ssl/example.com, api.cryptic.icu/webshot?https://example.com, api by https://frogiee.one'});
     
 });
 
@@ -28,17 +28,16 @@ app.get('/domain/ssl/:hostname', async (req, res) => {
         const response = await axios.get(`https://networkcalc.com/api/security/certificate/${hostname}`);
         res.json(response.data);
     } catch (error) {
-        // Handle errors
         console.error(error);
         res.status(500).json({ error: 'An error occurred while processing your request' });
     }
 });
 
-app.get('/webshot/:hostname', async (req, res) => {
-    const { hostname } = req.params;
+app.get('/webshot', async (req, res) => {
+    const hostname = req.query.url;
 
     try {
-        const response = await axios.get(`https://image.thum.io/get/width/1000/crop/700/http://${hostname}`, {
+        const response = await axios.get(`https://image.thum.io/get/width/1000/crop/700/` + hostname, {
             responseType: 'arraybuffer'
         });
         res.setHeader('Content-Type', 'image/png');
